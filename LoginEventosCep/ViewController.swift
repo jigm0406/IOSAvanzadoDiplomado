08010c4b879c6a4dedf245b5b280c2adbe99e92f
Mario Jimenez <mariojimenez@la-macbook-pro-de-mario.local>
2016-11-15 16:43:03 -0600

    Initial Commit

---
+++ LoginEventosCep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginEventosCep
+//
+//  Created by Mario Jimenez on 15/11/16.
+//  Copyright © 2016 Mario Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
